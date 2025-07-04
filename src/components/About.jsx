a0fa671130b6f27639d713582f62cc2bf2403fdc
Alish-Shakya <alishshakya44@gmail.com>
2025-07-04 23:15:38 +0545

    tailwind config

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const About = () => {
-  return <div>This is my About component</div>;
+  return (
+    <>
+      <div className="text-2xl">This is my About component</div>
+      <p>my name is Alish and this is my tailwind test</p>;
+    </>
+  );
 };
 
 export default About;
